558762994eddd8d04e55cbcc2d538b71d0c72c9d
Jakob-Ratajc-Nti-Johanneberg <95360400+Jakob-Ratajc-Nti-Johanneberg@users.noreply.github.com>
2022-04-03 23:17:26 +0200

    almost finished

--- js/scrips.js
+++ js/scrips.js
@@ -1,14 +1,30 @@
-scroller = document.getElementById('mainpage');
-var nav = document.getElementById('navigation')
 
 
-var myScrollFunc = function() {
-    var y = window.scrollY;
-    if (y >= 70) //{
-      nav.style.right= 1000+"px";
-    } else {
-      nav.style.right= 0;
+  function replace( hide, hide2, hide3,  show ) {
+    document.getElementById(hide).style.display="none";
+    document.getElementById(show).style.display="inline-flex";
+    document.getElementById(hide2).style.display="none";
+    document.getElementById(hide3).style.display="none";
+  }
+
+
+  function hidediv(hide, show, show2 ,show3){
+    document.getElementById(hide).style.display="none";
+    document.getElementById(show).style.display="inline-flex";
+    document.getElementById(show2).style.display="inline-flex";
+    document.getElementById(show3).style.display="inline-flex";
+  }
+
+  var button = document.querySelector("#hamburger");
+  var menu = document.querySelector("#navigation");
+
+  button.addEventListener("click", () =>{
+    if (menu.style.display === "none"){
+      menu.style.display = "flex";
+      button.innerHTML = "Close";
     }
-  };
-  
-  window.addEventListener("scroll", myScrollFunc);
+    else{
+      menu.style.display = "none";
+      button.innerHTML = "Menu";
+    }
+  })
